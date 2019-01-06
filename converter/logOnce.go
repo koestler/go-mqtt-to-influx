01991430e884323c1e61d33be338bbea98090974
Lorenz Koestler <lorenz@koestler.ch>
2019-01-06 23:15:54 +0100

    logOnce: bugfix: prevent concurrent map read and map write

--- converter/logOnce.go
+++ converter/logOnce.go
@@ -3,15 +3,25 @@ package converter
 import (
 	"fmt"
 	"log"
+	"sync"
 )
 
-var loggedTopics = make(map[string]bool)
+var (
+	loggedTopics     = make(map[string]bool)
+	loggedTopicMutex sync.RWMutex
+)
 
 func logTopicOnce(converter, topic string) {
 	id := fmt.Sprintf("%s,%s", converter, topic)
+
+	loggedTopicMutex.RLock()
 	_, ok := loggedTopics[id]
+	loggedTopicMutex.RUnlock()
+
 	if !ok {
 		log.Printf("converter[%s]: handle topic='%s'", converter, topic)
+		loggedTopicMutex.Lock()
 		loggedTopics[id] = true
+		loggedTopicMutex.Unlock()
 	}
 }
