ce198da55f476fb8a371443c5abe7faa73f91413
Lorenz Koestler <lorenz@koestler.ch>
2019-01-07 21:48:37 +0100

    bugfix: prevent reace condition

--- converter/logOnce.go
+++ converter/logOnce.go
@@ -8,20 +8,17 @@ import (
 
 var (
 	loggedTopics     = make(map[string]bool)
-	loggedTopicMutex sync.RWMutex
+	loggedTopicMutex sync.Mutex
 )
 
 func logTopicOnce(converter string, input Input) {
 	id := fmt.Sprintf("%s,%s", converter, input.Topic())
 
-	loggedTopicMutex.RLock()
-	_, ok := loggedTopics[id]
-	loggedTopicMutex.RUnlock()
+	loggedTopicMutex.Lock()
+	defer loggedTopicMutex.Unlock()
 
-	if !ok {
+	if _, ok := loggedTopics[id]; !ok {
 		log.Printf("converter[%s]: handle topic='%s', payload='%s'", converter, input.Topic(), input.Payload())
-		loggedTopicMutex.Lock()
 		loggedTopics[id] = true
-		loggedTopicMutex.Unlock()
 	}
 }
