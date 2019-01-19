fa3dd07b68bd6abba87a910551aa47457744a977
Lorenz Koestler <lorenz@koestler.ch>
2019-01-19 16:07:36 +0100

    mqtt: replace placeholders in topic

--- mqttClient/mqttClient.go
+++ mqttClient/mqttClient.go
@@ -110,7 +110,10 @@ func (mq *MqttClient) wrapCallBack(callback mqtt.MessageHandler) mqtt.MessageHan
 
 func (mq *MqttClient) Subscribe(topic string, callback mqtt.MessageHandler) error {
 	log.Printf("mqttClient[%s]: subscribe to topic='%s'", mq.Name(), topic)
-	if token := mq.client.Subscribe(topic, mq.config.Qos(), mq.wrapCallBack(callback)); token.Wait() && token.Error() != nil {
+	if token := mq.client.Subscribe(
+		replaceTemplate(topic, mq.config),
+		mq.config.Qos(), mq.wrapCallBack(callback),
+	); token.Wait() && token.Error() != nil {
 		return token.Error()
 	}
 	return nil
