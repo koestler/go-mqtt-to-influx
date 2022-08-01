3a42f910fc629a0fab7442279de22eb4680fc858
Lorenz Koestler <lorenz@koestler.ch>
2022-08-01 22:32:10 +0200

    remove unused build script

--- build.sh
+++
@@ -1,3 +0,0 @@
-VERSION=`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`
-OOS=linux go build -ldflags="-s -w -X main.buildVersion=$VERSION -X main.buildTime=`date -Is`"
-upx -qqq go-mqtt-to-influx
